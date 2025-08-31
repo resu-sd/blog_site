9a7f6d3eba32b5f93d5598534ce08a420d74ca2e
resu <>
2025-08-31 21:41:09 +0545

    Fix templates dirs

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'blog_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
